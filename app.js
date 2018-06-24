2cbf5c9366a606de674ac3f84bfa8e6c0c1bdfd6
sumitt12690 <sumitt12690@gmail.com>
2018-06-24 23:05:35 +0530

    same work done with nodejs. code commented

--- app.js
+++ app.js
@@ -16,3 +16,24 @@ io.on('connection', function(socket){
 http.listen(3002,function() {
     console.log('listening on port 3002');
 })
+
+
+// var fs=require('fs');
+// var userView = function(req, res) {
+//     fs.readFile('./index.html', function (err, data) {
+//         if(err) throw err;
+//         res.writeHead(200);
+//         res.end(data);
+//     });
+// }
+// var server = require('http').createServer(userView);
+// var io = require('socket.io')(server);
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//         io.emit('chat message', msg);
+//       });
+// });
+
+// server.listen(3002,function() {
+//     console.log('listening on port 3002');
+// })
